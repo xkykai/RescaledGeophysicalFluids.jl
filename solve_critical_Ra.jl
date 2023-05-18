7592c8d39b0796e1f1b29f3f6e41bb18b4c011e0
Xin Kai Lee <xinkai@mit.edu>
2023-05-18 15:41:09 -0400

    find critical Ra and k for all 10 combinations

--- solve_critical_Ra.jl
+++ solve_critical_Ra.jl
@@ -69,13 +69,11 @@ struct B_bc{BC, C}
     coef :: C
 end
 
-struct FluxBoundaryCondition{V, Z}
-    value :: V
+struct FluxBoundaryCondition{Z}
     z :: Z
 end
 
-struct ValueBoundaryCondition{V, Z}
-    value :: V
+struct ValueBoundaryCondition{Z}
     z :: Z
 end
 
@@ -119,14 +117,14 @@ function B_bc(bc::ValueBoundaryCondition, us, ms)
     return B_bc(bc, coef)
 end
 
-uv_bc_top = FluxBoundaryCondition(0, H/2)
-uv_bc_bot = FluxBoundaryCondition(0, -H/2)
+uv_bc_top = FluxBoundaryCondition(H/2)
+uv_bc_bot = FluxBoundaryCondition(-H/2)
 
-w_bc_top = ValueBoundaryCondition(0, H/2)
-w_bc_bot = ValueBoundaryCondition(0, -H/2)
+w_bc_top = ValueBoundaryCondition(H/2)
+w_bc_bot = ValueBoundaryCondition(-H/2)
 
-b_bc_top = ValueBoundaryCondition(0, H/2)
-b_bc_bot = ValueBoundaryCondition(S, -H/2)
+b_bc_top = ValueBoundaryCondition(H/2)
+b_bc_bot = ValueBoundaryCondition(-H/2)
 
 function build_bc_matrix(uv_bc_top, uv_bc_bot, w_bc_top, w_bc_bot, b_bc_top, b_bc_bot, us, ms)
     U_bc_top = U_bc(uv_bc_top, us, ms)
@@ -159,18 +157,16 @@ function det_bc_matrix(k, Ra, Ta, Pr)
 
     us = compute_coefficients(k, ms, ν, κ, f, S, ρ₀)
 
-    uv_bc_top = ValueBoundaryCondition(0, H/2)
-    uv_bc_bot = ValueBoundaryCondition(0, -H/2)
+    uv_bc_top = ValueBoundaryCondition(H/2)
+    uv_bc_bot = ValueBoundaryCondition(-H/2)
 
-    w_bc_top = ValueBoundaryCondition(0, H/2)
-    w_bc_bot = ValueBoundaryCondition(0, -H/2)
+    w_bc_top = ValueBoundaryCondition(H/2)
+    w_bc_bot = ValueBoundaryCondition(-H/2)
 
-    b_bc_top = ValueBoundaryCondition(0, H/2)
-    b_bc_bot = ValueBoundaryCondition(S, -H/2)
+    b_bc_top = ValueBoundaryCondition(H/2)
+    b_bc_bot = ValueBoundaryCondition(-H/2)
 
     A = build_bc_matrix(uv_bc_top, uv_bc_bot, w_bc_top, w_bc_bot, b_bc_top, b_bc_bot, us, ms)
-
-    b = [uv_bc_top.value, uv_bc_top.value, w_bc_top.value, b_bc_top.value, uv_bc_bot.value, uv_bc_bot.value, w_bc_bot.value, b_bc_bot.value]
 
     return logabsdet(A)[1]
 end
@@ -193,9 +189,9 @@ function find_critical_Ra(k, bcs_type, Ta, Pr, Ra_min, Ra_max; ν=1, H=1, ρ₀=
         m₁⁺, m₁⁻, m₂⁺, m₂⁻, m₃⁺, m₃⁻ = [ms_eq[i](k, Ra, Ta) for i in 1:6]
         ms = [m₀⁺, m₀⁻, m₁⁺, m₁⁻, m₂⁺, m₂⁻, m₃⁺, m₃⁻]
 
-        bcs = (; uv = (; top=bcs_type.uv.top(0, H/2), bot=bcs_type.uv.bot(0, -H/2)),
-                  w = (; top=bcs_type.w.top(0, H/2), bot=bcs_type.w.bot(0, -H/2)),
-                  b = (; top=bcs_type.b.top(0, H/2), bot=bcs_type.b.bot(S*H, -H/2)))
+        bcs = (; uv = (; top=bcs_type.uv.top(H/2), bot=bcs_type.uv.bot(-H/2)),
+                  w = (; top=bcs_type.w.top(H/2), bot=bcs_type.w.bot(-H/2)),
+                  b = (; top=bcs_type.b.top(H/2), bot=bcs_type.b.bot(-H/2)))
 
         coeffs = compute_coefficients(k, ms, ν, κ, f, S, ρ₀)
         A = build_bc_matrix(bcs.uv.top, bcs.uv.bot, bcs.w.top, bcs.w.bot, bcs.b.top, bcs.b.bot, coeffs, ms)
@@ -226,35 +222,155 @@ function build_bc_matrix(k, bcs_type, Ta, Pr, Ra; ν=1, H=1, ρ₀=1000)
     
     ms = [m₀⁺, m₀⁻, m₁⁺, m₁⁻, m₂⁺, m₂⁻, m₃⁺, m₃⁻]
 
-    bcs = (; uv = (; top=bcs_type.uv.top(0, H/2), bot=bcs_type.uv.bot(0, -H/2)),
-              w = (; top=bcs_type.w.top(0, H/2), bot=bcs_type.w.bot(0, -H/2)),
-              b = (; top=bcs_type.b.top(0, H/2), bot=bcs_type.b.bot(S*H, -H/2)))
+    bcs = (; uv = (; top=bcs_type.uv.top(H/2), bot=bcs_type.uv.bot(-H/2)),
+              w = (; top=bcs_type.w.top(H/2), bot=bcs_type.w.bot(-H/2)),
+              b = (; top=bcs_type.b.top(H/2), bot=bcs_type.b.bot(-H/2)))
 
     coeffs = compute_coefficients(k, ms, ν, κ, f, S, ρ₀)
 
-    A = build_bc_matrix(uv_bc_top, uv_bc_bot, w_bc_top, w_bc_bot, b_bc_top, b_bc_bot, coeffs, ms)
+    A = build_bc_matrix(bcs.uv.top, bcs.uv.bot, bcs.w.top, bcs.w.bot, bcs.b.top, bcs.b.bot, coeffs, ms)
     return A
 end
 
-# bcs_type = (; uv = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition), 
-#                w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
-#                b = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition))
+##
+function find_plot_critical_Ra_k_A(bcs_type, ks, Ta, Pr; Ra_min, Ra_max, ν=1, H=1, ρ₀=1000)
+    @info "Finding critical Ra for each k..."
+    res_Ras = [find_critical_Ra(k, bcs_type, Ta, Pr, Ra_min, Ra_max, ν=ν, H=H, ρ₀=ρ₀) for k in ks]
 
-bcs_type = (; uv = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition), 
-               w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
-               b = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition))
+    @info "Finding critical and k..."
+    res_Ra_k = find_critical_Ra_k(ks[1], ks[end], bcs_type, Ta, Pr, Ra_min, Ra_max)
 
+    k′ = res_Ra_k.minimizer
+    Ra′ = res_Ra_k.minimum
 
-ks = 1:0.01:5
-res_Ras = [find_critical_Ra(k, bcs_type, Ta, Pr, Ta*1.5, 1e4) for k in ks]
-res_Ra_k = find_critical_Ra_k(ks[1], ks[end], bcs_type, Ta, Pr, Ta*1.5, 1e4)
+    @info "Plotting results..."
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="k", ylabel="Critical Ra", title="uv: $(bcs_type.uv), \n w: $(bcs_type.w), \n b: $(bcs_type.b)")
+    lines!(ax, ks, res_Ras)
+    scatter!(ax, [k′], [Ra′], color=:black, label="k = $(round(k′, digits=2)), Ra = $(round(Ra′, digits=2))")
+    axislegend(ax)
+    display(fig)
+
+    @info "Building matrix"
+    A = build_bc_matrix(k′, bcs_type, Ta, Pr, Ra′; ν=1, H=1, ρ₀=1000)
+    A_eigen = eigen(A)
+
+    m₀⁺′, m₀⁻′ = k′, -k′
+    m₁⁺′, m₁⁻′, m₂⁺′, m₂⁻′, m₃⁺′, m₃⁻′ = [ms_eq[i](Complex(k′), Ra′, Ta) for i in 1:6]
+
+    m′s = [m₀⁺′, m₀⁻′, m₁⁺′, m₁⁻′, m₂⁺′, m₂⁻′, m₃⁺′, m₃⁻′]
+
+    return res_Ra_k, fig, A, A_eigen, k′, m′s
+end
+##
+# uv: top flux, bottom flux, b: top value, bottom value
+bcs_type_uv_tfbf_b_tvbv = (; uv = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition))
+                              
+ks_uv_tfbf_b_tvbv = 1.5:0.01:5
+res_Ra_k_uv_tfbf_b_tvbv, fig_uv_tfbf_b_tvbv, A_uv_tfbf_b_tvbv, A_eigen_uv_tfbf_b_tvbv, k′_uv_tfbf_b_tvbv, m′s_uv_tfbf_b_tvbv = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbf_b_tvbv, ks_uv_tfbf_b_tvbv, Ta, Pr; Ra_min=Ta*1.5, Ra_max=1e4)
+
+save("Output/Rac_k_uv_tfbf_b_tvbv.png", fig_uv_tfbf_b_tvbv, px_per_unit=4)
 
 ##
-fig = Figure()
-ax = Axis(fig[1, 1], xlabel="k", ylabel="Critical Ra")
-lines!(ax, ks, res_Ras)
-display(fig)
+# uv: top value, bottom value, b: top value, bottom value
+bcs_type_uv_tvbv_b_tvbv = (; uv = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition))
+
+ks_uv_tvbv_b_tvbv = 1.5:0.01:5
+res_Ra_k_uv_tvbv_b_tvbv, fig_uv_tvbv_b_tvbv, A_uv_tvbv_b_tvbv, A_eigen_uv_tvbv_b_tvbv, k′_uv_tvbv_b_tvbv, m′s_uv_tvbv_b_tvbv = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tvbv_b_tvbv, ks_uv_tvbv_b_tvbv, Ta, Pr; Ra_min=Ta*1.5, Ra_max=1e4)
+                              
+save("Output/Rac_k_uv_tvbv_b_tvbv.png", fig_uv_tvbv_b_tvbv, px_per_unit=4)
 ##
-A = build_bc_matrix(res_Ra_k.minimizer, bcs_type, Ta, Pr, res_Ra_k.minimum; ν=1, H=1, ρ₀=1000)
-eigen(A)
+# uv: top flux, bottom value, b: top value, bottom value
+bcs_type_uv_tfbv_b_tvbv = (; uv = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition))
+
+ks_uv_tfbv_b_tvbv = 1.5:0.01:4.5
+res_Ra_k_uv_tfbv_b_tvbv, fig_uv_tfbv_b_tvbv, A_uv_tfbv_b_tvbv, A_eigen_uv_tfbv_b_tvbv, k′_uv_tfbv_b_tvbv, m′s_uv_tfbv_b_tvbv = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbv_b_tvbv, ks_uv_tfbv_b_tvbv, Ta, Pr; Ra_min=Ta*1.5, Ra_max=1e4)
+                              
+save("Output/Rac_k_uv_tfbv_b_tvbv.png", fig_uv_tfbv_b_tvbv, px_per_unit=4)
+##
+# uv: top flux, bottom flux, b: top flux, bottom flux
+bcs_type_uv_tfbf_b_tfbf = (; uv = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition))
+                              
+ks_uv_tfbf_b_tfbf = 3:0.01:5
+res_Ra_k_uv_tfbf_b_tfbf, fig_uv_tfbf_b_tfbf, A_uv_tfbf_b_tfbf, A_eigen_uv_tfbf_b_tfbf, k′_uv_tfbf_b_tfbf, m′s_uv_tfbf_b_tfbf = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbf_b_tfbf, ks_uv_tfbf_b_tfbf, Ta, Pr; Ra_min=Ta*1.5, Ra_max=1e4)
+
+save("Output/Rac_k_uv_tfbf_b_tfbf.png", fig_uv_tfbf_b_tfbf, px_per_unit=4)
+##
+# uv: top value, bottom value, b: top flux, bottom flux
+bcs_type_uv_tvbv_b_tfbf = (; uv = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition))
+
+ks_uv_tvbv_b_tfbf = 3:0.01:4.5
+res_Ra_k_uv_tvbv_b_tfbf, fig_uv_tvbv_b_tfbf, A_uv_tvbv_b_tfbf, A_eigen_uv_tvbv_b_tfbf, k′_uv_tvbv_b_tfbf, m′s_uv_tvbv_b_tfbf = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tvbv_b_tfbf, ks_uv_tvbv_b_tfbf, Ta, Pr; Ra_min=Ta*1.2, Ra_max=2e4)
+                              
+save("Output/Rac_k_uv_tvbv_b_tfbf.png", fig_uv_tvbv_b_tfbf, px_per_unit=4)
+##
+# uv: top flux, bottom value, b: top flux, bottom flux
+bcs_type_uv_tfbv_b_tfbf = (; uv = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition))
+
+ks_uv_tfbv_b_tfbf = 3:0.01:4.5
+res_Ra_k_uv_tfbv_b_tfbf, fig_uv_tfbv_b_tfbf, A_uv_tfbv_b_tfbf, A_eigen_uv_tfbv_b_tfbf, k′_uv_tfbv_b_tfbf, m′s_uv_tfbv_b_tfbf = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbv_b_tfbf, ks_uv_tfbv_b_tfbf, Ta, Pr; Ra_min=Ta*1.5, Ra_max=2e4)
+                              
+save("Output/Rac_k_uv_tfbv_b_tfbf.png", fig_uv_tvbv_b_tfbf, px_per_unit=4)
+##
+# uv: top flux, bottom flux, b: top value, bottom flux
+bcs_type_uv_tfbf_b_tvbf = (; uv = (; top=FluxBoundaryCondition, bot=FluxBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=ValueBoundaryCondition, bot=FluxBoundaryCondition))
+                              
+ks_uv_tfbf_b_tvbf = 3.5:0.01:5
+res_Ra_k_uv_tfbf_b_tvbf, fig_uv_tfbf_b_tvbf, A_uv_tfbf_b_tvbf, A_eigen_uv_tfbf_b_tvbf, k′_uv_tfbf_b_tvbf, m′s_uv_tfbf_b_tvbf = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbf_b_tvbf, ks_uv_tfbf_b_tvbf, Ta, Pr; Ra_min=Ta*1.5, Ra_max=2e4)
+
+save("Output/Rac_k_uv_tfbf_b_tvbf.png", fig_uv_tfbf_b_tvbf, px_per_unit=4)
+##
+# uv: top value, bottom value, b: top flux, bottom value
+bcs_type_uv_tvbv_b_tfbv = (; uv = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition))
+
+ks_uv_tvbv_b_tfbv = 3.5:0.01:5
+res_Ra_k_uv_tvbv_b_tfbv, fig_uv_tvbv_b_tfbv, A_uv_tvbv_b_tfbv, A_eigen_uv_tvbv_b_tfbv, k′_uv_tvbv_b_tfbv, m′s_uv_tvbv_b_tfbv = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tvbv_b_tfbv, ks_uv_tvbv_b_tfbv, Ta, Pr; Ra_min=Ta, Ra_max=5e4)
+                              
+save("Output/Rac_k_uv_tvbv_b_tfbv.png", fig_uv_tvbv_b_tfbv, px_per_unit=4)
+##
+# uv: top flux, bottom value, b: top flux, bottom value
+bcs_type_uv_tfbv_b_tfbv = (; uv = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition))
+
+ks_uv_tfbv_b_tfbv = 3:0.01:5.5
+res_Ra_k_uv_tfbv_b_tfbv, fig_uv_tfbv_b_tfbv, A_uv_tfbv_b_tfbv, A_eigen_uv_tfbv_b_tfbv, k′_uv_tfbv_b_tfbv, m′s_uv_tfbv_b_tfbv = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbv_b_tfbv, ks_uv_tfbv_b_tfbv, Ta, Pr; Ra_min=Ta, Ra_max=5e4)
+                              
+save("Output/Rac_k_uv_tfbv_b_tfbv.png", fig_uv_tfbv_b_tfbv, px_per_unit=4)
+##
+# uv: top flux, bottom value, b: top value, bottom flux
+bcs_type_uv_tfbv_b_tvbf = (; uv = (; top=FluxBoundaryCondition, bot=ValueBoundaryCondition), 
+                              w = (; top=ValueBoundaryCondition, bot=ValueBoundaryCondition),
+                              b = (; top=ValueBoundaryCondition, bot=FluxBoundaryCondition))
+
+ks_uv_tfbv_b_tvbf = 1:0.01:3.5
+res_Ra_k_uv_tfbv_b_tvbf, fig_uv_tfbv_b_tvbf, A_uv_tfbv_b_tvbf, A_eigen_uv_tfbv_b_tvbf, k′_uv_tfbv_b_tvbf, m′s_uv_tfbv_b_tvbf = find_plot_critical_Ra_k_A(
+    bcs_type_uv_tfbv_b_tvbf, ks_uv_tfbv_b_tvbf, Ta, Pr; Ra_min=Ta*1.2, Ra_max=1e4)
+                              
+save("Output/Rac_k_uv_tfbv_b_tvbf.png", fig_uv_tfbv_b_tvbf, px_per_unit=4)
 ##
