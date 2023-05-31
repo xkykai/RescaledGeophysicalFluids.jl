13a3cf4abc0618b3ee0c2893525476acecd65f42
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-05-31 12:00:20 +0100

    include PV in 2D rotating RB

--- 2D_rotating_rayleighbenard.jl
+++ 2D_rotating_rayleighbenard.jl
@@ -7,19 +7,18 @@ using Printf
 using CairoMakie
 using Oceananigans.Grids: halo_size
 
-const aspect_ratio = 1
+const aspect_ratio = 0.25
 
 const Lz = 1meter    # depth [m]
 const Lx = Lz / aspect_ratio # north-south extent [m]
 
-const Nz = 256
+const Nz = 64
 const Nx = Int(Nz / aspect_ratio)
 
 const Pr = 1
 const ν = 1
 const κ = ν / Pr
 
-# const Ra = 2151.3411993087107
 const Ra = 10000
 const S = Ra * ν * κ / Lz ^ 4
 
@@ -54,12 +53,12 @@ model = NonhydrostaticModel(;
 
 set!(model, b=b_initial)
 
-# simulation = Simulation(model, Δt=1e-6second, stop_iteration=200)
-simulation = Simulation(model, Δt=0.2e-6second, stop_time=6seconds)
+# simulation = Simulation(model, Δt=1e-6second, stop_iteration=2000)
+simulation = Simulation(model, Δt=1e-6second, stop_time=6seconds)
 
 # simulation.stop_iteration = 30000
 
-wizard = TimeStepWizard(max_change=1.05, max_Δt=1e-6, cfl=0.6)
+wizard = TimeStepWizard(max_change=1.05, max_Δt=5e-6, cfl=0.6)
 # simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(1))
 
@@ -99,6 +98,14 @@ end
 b = model.tracers.b
 u, v, w = model.velocities
 
+∂x_v = ∂x(v)
+∂z_v = ∂z(v)
+
+∂x_b = ∂x(b)
+∂z_b = ∂z(b)
+
+PV = ∂x_v * ∂z_b - ∂z_v * ∂x_b + f * ∂z_b
+
 B = Average(b, dims=(1, 2))
 U = Average(u, dims=(1, 2))
 V = Average(v, dims=(1, 2))
@@ -116,11 +123,11 @@ VB = Average(v * b, dims=(1, 2))
 UU = Average(u * u, dims=(1, 2))
 UB = Average(u * b, dims=(1, 2))
 
-field_outputs = merge(model.velocities, model.tracers)
+field_outputs = merge(model.velocities, model.tracers, (; PV))
 
 simulation.output_writers[:jld2] = JLD2OutputWriter(model, field_outputs,
                                                           filename = "$(FILE_DIR)/instantaneous_fields.jld2",
-                                                          schedule = TimeInterval(2e-3),
+                                                          schedule = TimeInterval(5e-3),
                                                           # schedule = IterationInterval(10),
                                                           with_halos = true,
                                                         #   overwrite_existing = true,
@@ -129,7 +136,7 @@ simulation.output_writers[:jld2] = JLD2OutputWriter(model, field_outputs,
 simulation.output_writers[:timeseries] = JLD2OutputWriter(model, (; B, U, V, W, UW, VW, WW, WB, UV, VV, VB, UU, UB),
                                                           filename = "$(FILE_DIR)/instantaneous_timeseries.jld2",
                                                           # schedule = IterationInterval(10),
-                                                          schedule = TimeInterval(2e-3),
+                                                          schedule = TimeInterval(5e-3),
                                                           with_halos = true,
                                                         #   overwrite_existing = true,
                                                           init = init_save_some_metadata!)
@@ -145,7 +152,11 @@ simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=TimeInte
 # run!(simulation, pickup="$(FILE_DIR)/model_checkpoint_iteration10000.jld2")
 run!(simulation)
 
-b_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_fields.jld2", "b")
+data = FieldDataset("$(FILE_DIR)/instantaneous_fields.jld2")
+
+b_data = data["b"]
+PV_data = data["PV"]
+
 B_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "B")
 WB_data = FieldTimeSeries("$(FILE_DIR)/instantaneous_timeseries.jld2", "WB")
 
@@ -153,33 +164,43 @@ Nu_data = WB_data ./ (κ * S)
 
 Nt = length(b_data.times)
 
-xC = B_data.grid.xᶜᵃᵃ[1:Nx]
-zC = B_data.grid.zᵃᵃᶜ[1:Nz]
-zF = WB_data.grid.zᵃᵃᶠ[1:Nz+1]
+xb, yb, zb = nodes(b_data)
+xPV, yPV, zPV = nodes(PV_data)
 
-fig = Figure(resolution=(1500, 1500))
+xB, yB, zB = nodes(B_data)
+xNu, yNu, zNu = nodes(WB_data)
+
+##
+fig = Figure(resolution=(1200, 1500))
 
 slider = Slider(fig[0, 1:2], range=1:Nt, startvalue=1)
 n = slider.value
 
 axb = Axis(fig[1, 1:2], title="b", xlabel="x", ylabel="z")
-axB = Axis(fig[2, 1], title="<b>", xlabel="<b>", ylabel="z")
-axNu = Axis(fig[2, 2], title="Nu", xlabel="Nu", ylabel="z")
+axPV = Axis(fig[2, 1:2], title="PV", xlabel="x", ylabel="z")
+
+axB = Axis(fig[3, 1], title="<b>", xlabel="<b>", ylabel="z")
+axNu = Axis(fig[3, 2], title="Nu", xlabel="Nu", ylabel="z")
 
 bn = @lift interior(b_data[$n], :, 1, :)
+PVn = @lift interior(PV_data[$n], :, 1, :)
+
 Bn = @lift interior(B_data[$n], 1, 1, :)
 Nun = @lift Nu_data[1, 1, :, $n]
 time_str = @lift "Ra = $(Ra), Ta = $(Ta), Pr = $(Pr), Time = $(round(b_data.times[$n], digits=2))"
 
 title = Label(fig[-1, 1:2], time_str, font=:bold)
 
-blim = (-1*maximum(abs, b_data), maximum(abs, b_data))
+blim = (minimum(b_data), maximum(b_data))
+PVlim = (minimum(PV_data), maximum(PV_data))
+
 Blim = (minimum(B_data), maximum(B_data))
 Nulim = (minimum(Nu_data), maximum(Nu_data))
 
-heatmap!(axb, xC, zC, bn, colormap=Reverse(:RdBu_10), colorrange=blim)
-lines!(axB, Bn, zC)
-lines!(axNu, Nun, zF)
+heatmap!(axb, xb, zb, bn, colormap=Reverse(:RdBu_10), colorrange=blim)
+heatmap!(axPV, xPV, zPV, PVn, colormap=Reverse(:RdBu_10), colorrange=PVlim)
+lines!(axB, Bn, zB)
+lines!(axNu, Nun, zNu)
 
 xlims!(axB, Blim)
 xlims!(axNu, Nulim)
@@ -187,3 +208,4 @@ xlims!(axNu, Nulim)
 record(fig, "$(FILE_DIR)/rayleighbenard_convection.mp4", 1:Nt, framerate=10) do nn
     n[] = nn
 end
+##
