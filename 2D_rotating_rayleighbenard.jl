8ad6046d7bfe506dbe91337bafb915272ea3c251
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-06-04 14:57:58 +0100

    add arguments to 2D rotating RB

--- 2D_rotating_rayleighbenard.jl
+++ 2D_rotating_rayleighbenard.jl
@@ -6,30 +6,94 @@ using Printf
 # using GLMakie
 using CairoMakie
 using Oceananigans.Grids: halo_size
+using ArgParse
+using RescaledGeophysicalFluids
 
-const aspect_ratio = 0.125
+function parse_commandline()
+  s = ArgParseSettings()
+
+  @add_arg_table! s begin
+    "--rayleigh_number"
+      help = "Rayleigh Number"
+      arg_type = Float64
+      default = 2000.
+    "--taylor_number"
+      help = "Taylor Number"
+      arg_type = Float64
+      default = 0.
+    "--prandtl_number"
+      help = "Prandtl Number"
+      arg_type = Float64
+      default = 1.
+    "--aspect_ratio"
+      help = "aspect ratio alpha = H / L, H = 1"
+      arg_type = Float64
+      default = 0.125
+    "--dt"
+      help = "Initial timestep to take (seconds)"
+      arg_type = Float64
+      default = 1e-6
+    "--max_dt"
+      help = "Maximum timestep (seconds)"
+      arg_type = Float64
+      default = 1e-4
+    "--stop_time"
+      help = "Stop time of simulation (seconds)"
+      arg_type = Float64
+      default = 3.
+    "--Nz"
+      help = "Number of grid points in z-direction"
+      arg_type = Int64
+      default = 32
+    "--uv_top"
+      help = "Type of boundary condition for velocities at the top"
+      arg_type = String
+      default = "f"
+    "--uv_bot"
+      help = "Type of boundary condition for velocities at the bottom"
+      arg_type = String
+      default = "f"
+    "--b_top"
+      help = "Type of boundary condition for buoyancy at the top"
+      arg_type = String
+      default = "v"
+    "--b_bot"
+      help = "Type of boundary condition for buoyancy at the bottom"
+      arg_type = String
+      default = "v"
+  end
+  return parse_args(s)
+end
+
+args = parse_commandline()
+
+const aspect_ratio = args["aspect_ratio"]
 
 const Lz = 1meter    # depth [m]
 const Lx = Lz / aspect_ratio # north-south extent [m]
 
-const Nz = 64
-const Nx = Int(Nz / aspect_ratio)
+const Nz = args["Nz"]
+const Nx = Int(round(Nz / aspect_ratio))
 
-const Pr = 1
+const Pr = args["prandtl_number"]
 const ν = 1
 const κ = ν / Pr
 
-const Ra = 5000
+const Ra = args["rayleigh_number"]
 const S = Ra * ν * κ / Lz ^ 4
 
-const Ta = 1000
+const Ta = args["taylor_number"]
 const f = √(Ta * ν ^ 2 / Lz ^ 4)
 
-uv_bc_top_type = "f"
-uv_bc_bot_type = "f"
+uv_bc_top_type = args["uv_top"]
+uv_bc_bot_type = args["uv_bot"]
 
-b_bc_top_type = "v"
-b_bc_bot_type = "v"
+b_bc_top_type = args["b_top"]
+b_bc_bot_type = args["b_bot"]
+
+Δt = args["dt"]
+max_Δt = args["max_dt"]
+stop_time = args["stop_time"]
 
 FILE_DIR = "Data/2D_no_wind_uv_t$(uv_bc_top_type)b$(uv_bc_bot_type)_b_t$(b_bc_top_type)b$(b_bc_bot_type)_Ra_$(Ra)_Ta_$(Ta)_alpha_$(aspect_ratio)_Nz_$(Nz)"
 mkpath(FILE_DIR)
@@ -66,7 +130,6 @@ else
 end
 
 uv_bcs = FieldBoundaryConditions(top=uv_bc_top, bottom=uv_bc_bot)
-
 b_bcs = FieldBoundaryConditions(top=b_bc_top, bottom=b_bc_bot)
 
 b_initial(x, y, z) = -rand() * Ra / 100000
@@ -85,11 +148,11 @@ model = NonhydrostaticModel(;
 set!(model, b=b_initial)
 
 # simulation = Simulation(model, Δt=2e-6second, stop_iteration=2000)
-simulation = Simulation(model, Δt=1e-6second, stop_time=3seconds)
+simulation = Simulation(model, Δt=Δt, stop_time=stop_time)
 
 # simulation.stop_iteration = 30000
 
-wizard = TimeStepWizard(max_change=1.05, max_Δt=5e-6, cfl=0.6)
+wizard = TimeStepWizard(max_change=1.05, max_Δt=max_Δt, cfl=0.6)
 # simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(1))
 
@@ -156,7 +219,7 @@ field_outputs = merge(model.velocities, model.tracers, (; PV))
 
 simulation.output_writers[:jld2] = JLD2OutputWriter(model, field_outputs,
                                                           filename = "$(FILE_DIR)/instantaneous_fields.jld2",
-                                                          schedule = TimeInterval(5e-3),
+                                                          schedule = TimeInterval(1e-2),
                                                           # schedule = IterationInterval(10),
                                                           with_halos = true,
                                                         #   overwrite_existing = true,
@@ -165,7 +228,7 @@ simulation.output_writers[:jld2] = JLD2OutputWriter(model, field_outputs,
 simulation.output_writers[:timeseries] = JLD2OutputWriter(model, (; B, U, V, W, UW, VW, WW, WB, UV, VV, VB, UU, UB),
                                                           filename = "$(FILE_DIR)/instantaneous_timeseries.jld2",
                                                           # schedule = IterationInterval(10),
-                                                          schedule = TimeInterval(5e-3),
+                                                          schedule = TimeInterval(1e-2),
                                                           with_halos = true,
                                                         #   overwrite_existing = true,
                                                           init = init_save_some_metadata!)
@@ -199,13 +262,19 @@ xPV, yPV, zPV = nodes(PV_data)
 xB, yB, zB = nodes(B_data)
 xNu, yNu, zNu = nodes(WB_data)
 
+bs_k = interior(b_data[end], :, 1, Int(floor(length(zb) / 2)))
+
+kc, kc_neighbourhood = calculate_critical_k(bs_k, xb)
+
+@info "Critical k = $(kc) in the neighbourhood of $(kc_neighbourhood)"
+
 ##
 fig = Figure(resolution=(1200, 1500))
 
 slider = Slider(fig[0, 1:2], range=1:Nt, startvalue=1)
 n = slider.value
 
-axb = Axis(fig[1, 1:2], title="b", xlabel="x", ylabel="z")
+axb = Axis(fig[1, 1:2], title="b, k = $(round(kc, digits=2)), neighbouring ks = $(round.(kc_neighbourhood, digits=2))", xlabel="x", ylabel="z")
 axPV = Axis(fig[2, 1:2], title="PV", xlabel="x", ylabel="z")
 
 axB = Axis(fig[3, 1], title="<b>", xlabel="<b>", ylabel="z")
@@ -234,7 +303,7 @@ lines!(axNu, Nun, zNu)
 xlims!(axB, Blim)
 xlims!(axNu, Nulim)
 
-record(fig, "$(FILE_DIR)/rayleighbenard_convection.mp4", 1:Nt, framerate=30) do nn
+record(fig, "$(FILE_DIR)/rayleighbenard_convection.mp4", 1:Nt, framerate=20) do nn
     n[] = nn
 end
 ##
