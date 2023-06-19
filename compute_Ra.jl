5d66416b4a54bdabefc030d604f7d05cfeceeace
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-06-19 22:42:56 +0100

    compute Ra on Ta=1e9

--- compute_Ra.jl
+++ compute_Ra.jl
@@ -1,6 +1,6 @@
 using Oceananigans
 
-FILE_DIR = "Data/2D_no_wind_uv_tfbf_b_tfbf_Ra_120000.0_Ta_1.0e6_alpha_0.125_Nz_64"
+FILE_DIR = "Data/2D_no_wind_uv_tfbf_b_tfbf_Ra_1.2e7_Ta_1.0e9_alpha_1.0_Nz_128"
 B_data = FieldTimeSeries("$(FILE_DIR)/averaged_timeseries.jld2", "B")
 
 Ra = B_data[1, 1, 1, end] - B_data[1, 1, end, end]
