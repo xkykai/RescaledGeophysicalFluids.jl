b419619d52057d0e2b1810010fbb4e38e0d8e299
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-06-28 11:13:40 +0800

    comparing v and geostrophic v

--- solve_critical_Ra_Ta1e3.jl
+++ solve_critical_Ra_Ta1e3.jl
@@ -24,13 +24,49 @@ res_Ra_k_uv_tfbf_b_tvbv, fig_uv_tfbf_b_tvbv, A_uv_tfbf_b_tvbv, A_eigen_uv_tfbf_b
 
 RHS_uv_tfbf_b_tvbv = [0, 0, 0, 0, 0, 0, 0, res_Ra_k_uv_tfbf_b_tvbv.minimum]
 
-pinv(A_uv_tfbf_b_tvbv) * RHS_uv_tfbf_b_tvbv
+P_uv_tfbf_b_tvbv = A_uv_tfbf_b_tvbv \ RHS_uv_tfbf_b_tvbv
 
-A_uv_tfbf_b_tvbv \ RHS_uv_tfbf_b_tvbv
+coeffs_uv_tfbf_b_tvbv = compute_coefficients(k′_uv_tfbf_b_tvbv, m′s_uv_tfbf_b_tvbv; Ra=res_Ra_k_uv_tfbf_b_tvbv.minimum, Ta=Ta, Pr=Pr, ρ₀=ρ₀, H=H, ν=ν)
 
+V_uv_tfbf_b_tvbv = [coeffs_uv_tfbf_b_tvbv[i][2] * P_uv_tfbf_b_tvbv[i] for i in eachindex(P_uv_tfbf_b_tvbv)]
+
+zs = -H/2:0.01:H/2
+xs = 0:0.01:4H
+
+vs_uv_tfbf_b_tvbv = zeros(length(xs), length(zs))
+for i in eachindex(xs), j in eachindex(zs)
+    vs_uv_tfbf_b_tvbv[i, j] = real(sum([V_uv_tfbf_b_tvbv[i′] * exp(m′s_uv_tfbf_b_tvbv[i′] * zs[j]) for i′ in eachindex(V_uv_tfbf_b_tvbv)]) * exp(im * k′_uv_tfbf_b_tvbv * xs[i]))
+end
+
+f = √(Ta * ν ^ 2 / H ^ 4)
+
+v_gs_uv_tfbf_b_tvbv = zeros(length(xs), length(zs))
+for i in eachindex(xs), j in eachindex(zs)
+    v_gs_uv_tfbf_b_tvbv[i, j] = real(im * k′_uv_tfbf_b_tvbv * exp(im * k′_uv_tfbf_b_tvbv * xs[i]) * 
+                                   sum([P_uv_tfbf_b_tvbv[i′] * exp(m′s_uv_tfbf_b_tvbv[i′] * zs[j]) for i′ in eachindex(P_uv_tfbf_b_tvbv)])) ./ f ./ ρ₀
+end
+
+##
+fig = Figure(resolution=(1000, 500))
+
+axv = Axis(fig[1, 1], title="v")
+axg = Axis(fig[1, 2], title="Geostrophic v")
+
+vlim = (minimum([minimum(vs_uv_tfbf_b_tvbv), minimum(v_gs_uv_tfbf_b_tvbv)]), 
+        maximum([maximum(vs_uv_tfbf_b_tvbv), maximum(v_gs_uv_tfbf_b_tvbv)]))
+
+heatmap!(axv, xs, zs, vs_uv_tfbf_b_tvbv, colorrange=vlim)
+heatmap!(axg, xs, zs, v_gs_uv_tfbf_b_tvbv, colorrange=vlim)
+
+Label(fig[0, :], "Free-slip, constant buoyancy", font=:bold)
+
+# heatmap!(axv, xs, zs, vs_uv_tfbf_b_tvbv)
+display(fig)
+save("v_v_g_uv_tfbf_b_tvbv.png", fig, px_per_unit=4)
+##
 # save("Output/Rac_k_uv_tfbf_b_tvbv.png", fig_uv_tfbf_b_tvbv, px_per_unit=4)
 
-# nonzero_m′s_uv_tfbf_b_tvbv, nonzero_eigvec_ind_uv_tfbf_b_tvbv = find_zero_eigenvalue(A_eigen_uv_tfbf_b_tvbv, m′s_uv_tfbf_b_tvbv)
+nonzero_m′s_uv_tfbf_b_tvbv, nonzero_eigvec_ind_uv_tfbf_b_tvbv = find_zero_eigenvalue(A_eigen_uv_tfbf_b_tvbv, m′s_uv_tfbf_b_tvbv)
 
 ##
 # uv: top value, bottom value, b: top value, bottom value
@@ -71,6 +107,49 @@ res_Ra_k_uv_tfbf_b_tfbf, fig_uv_tfbf_b_tfbf, A_uv_tfbf_b_tfbf, A_eigen_uv_tfbf_b
     bcs_type_uv_tfbf_b_tfbf, ms_eq, ks_uv_tfbf_b_tfbf, Ta, Pr; Ra_min=Ta*1.01, Ra_max=1500)
 
 # save("Output/Rac_k_uv_tfbf_b_tfbf.png", fig_uv_tfbf_b_tfbf, px_per_unit=4)
+
+RHS_uv_tfbf_b_tfbf = [0, 0, 0, -res_Ra_k_uv_tfbf_b_tfbf.minimum, 0, 0, 0, -res_Ra_k_uv_tfbf_b_tfbf.minimum]
+
+P_uv_tfbf_b_tfbf = A_uv_tfbf_b_tfbf \ RHS_uv_tfbf_b_tfbf
+
+coeffs_uv_tfbf_b_tfbf = compute_coefficients(k′_uv_tfbf_b_tfbf, m′s_uv_tfbf_b_tfbf; Ra=res_Ra_k_uv_tfbf_b_tfbf.minimum, Ta=Ta, Pr=Pr, ρ₀=ρ₀, H=H, ν=ν)
+
+V_uv_tfbf_b_tfbf = [coeffs_uv_tfbf_b_tfbf[i][2] * P_uv_tfbf_b_tfbf[i] for i in eachindex(P_uv_tfbf_b_tfbf)]
+
+zs = -H/2:0.01:H/2
+xs = 0:0.01:4H
+
+vs_uv_tfbf_b_tfbf = zeros(length(xs), length(zs))
+for i in eachindex(xs), j in eachindex(zs)
+    vs_uv_tfbf_b_tfbf[i, j] = real(sum([V_uv_tfbf_b_tfbf[i′] * exp(m′s_uv_tfbf_b_tfbf[i′] * zs[j]) for i′ in eachindex(V_uv_tfbf_b_tfbf)]) * exp(im * k′_uv_tfbf_b_tfbf * xs[i]))
+end
+
+f = √(Ta * ν ^ 2 / H ^ 4)
+
+v_gs_uv_tfbf_b_tfbf = zeros(length(xs), length(zs))
+for i in eachindex(xs), j in eachindex(zs)
+    v_gs_uv_tfbf_b_tfbf[i, j] = real(im * k′_uv_tfbf_b_tfbf * exp(im * k′_uv_tfbf_b_tfbf * xs[i]) * 
+                                   sum([P_uv_tfbf_b_tfbf[i′] * exp(m′s_uv_tfbf_b_tfbf[i′] * zs[j]) for i′ in eachindex(P_uv_tfbf_b_tfbf)])) ./ f ./ ρ₀
+end
+
+##
+fig = Figure(resolution=(1000, 500))
+
+axv = Axis(fig[1, 1], title="v")
+axg = Axis(fig[1, 2], title="Geostrophic v")
+
+vlim = (minimum([minimum(vs_uv_tfbf_b_tfbf), minimum(v_gs_uv_tfbf_b_tfbf)]), 
+        maximum([maximum(vs_uv_tfbf_b_tfbf), maximum(v_gs_uv_tfbf_b_tfbf)]))
+
+heatmap!(axv, xs, zs, vs_uv_tfbf_b_tfbf, colorrange=vlim)
+heatmap!(axg, xs, zs, v_gs_uv_tfbf_b_tfbf, colorrange=vlim)
+
+Label(fig[0, :], "Free-slip, constant buoyancy flux", font=:bold)
+
+# heatmap!(axv, xs, zs, vs_uv_tfbf_b_tfbf)
+display(fig)
+save("v_v_g_uv_tfbf_b_tfbf.png", fig, px_per_unit=4)
+##
 
 nonzero_m′s_uv_tfbf_b_tfbf, nonzero_eigvec_ind_uv_tfbf_b_tfbf = find_zero_eigenvalue(A_eigen_uv_tfbf_b_tfbf, m′s_uv_tfbf_b_tfbf)
 
